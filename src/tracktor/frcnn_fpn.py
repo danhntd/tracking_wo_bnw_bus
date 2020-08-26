433dca3773da6d68e78325961f14e42df0ce1e6e
danhntd <31885622+danhntd@users.noreply.github.com>
2020-08-26 22:24:45 +0700

    update classes id

--- src/tracktor/frcnn_fpn.py
+++ src/tracktor/frcnn_fpn.py
@@ -58,9 +58,10 @@ class FRCNN_FPN(FasterRCNN):
         # detections = detections[0]
         # return detections['boxes'].detach().cpu(), detections['scores'].detach().cpu()
 
-        pred_boxes = pred_boxes[:, 6:7].squeeze(dim=1).detach()
+        # ('background', 'motorcycle', 'car', 'bus', 'truck')
+        pred_boxes = pred_boxes[:, 3:4].squeeze(dim=1).detach()
         pred_boxes = resize_boxes(pred_boxes, self.preprocessed_images.image_sizes[0], self.original_image_sizes[0])
-        pred_scores = pred_scores[:, 6:7].squeeze(dim=1).detach()
+        pred_scores = pred_scores[:, 3:4].squeeze(dim=1).detach()
         return pred_boxes, pred_scores
 
     def load_image(self, images):
